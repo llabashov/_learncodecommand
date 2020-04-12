d3615581c13074c0ddd38e07941c46f9e9b7ff42
llabashov <llabashov@mail.ru>
2020-04-12 14:14:34 +0300

    edit repo

--- DjangoCommand.py
+++ DjangoCommand.py
@@ -1,7 +1,13 @@
 """
 python manage.py createsuperuser - создать админа для manage
 
+CSS бесплатные шаблоны
+Для установки Bootstrap тебе нужно добавить следующие строки в <head> 
+твоего .html файла (blog/templates/blog/post_list.html):
 
+blog/templates/blog/post_list.html
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
 
 
 """
--- gitcommand.py
+++ gitcommand.py
@@ -17,14 +17,4 @@
 #git push origin master - отправить в гитхаб
 
 
-"""
-CSS бесплатные шаблоны
-Для установки Bootstrap тебе нужно добавить следующие строки в <head> 
-твоего .html файла (blog/templates/blog/post_list.html):
-
-blog/templates/blog/post_list.html
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
-"""
-
 #git config --global --list - просмотр данных
