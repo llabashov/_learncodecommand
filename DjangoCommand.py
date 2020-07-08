0bc351f0d1204670aaa7067337e1fa33bd74bee1
llabashov <llabashov@mail.ru>
2020-07-08 17:46:16 +0300

    Add new code command

--- DjangoCommand.py
+++ DjangoCommand.py
@@ -1,5 +1,31 @@
 """
+pip install pylint-django - улучшение анализа кода Django
+
+django-admin startproject *name* - создать проект джанго
 python manage.py createsuperuser - создать админа для manage
+
+python manage.py startapp *name* - создать приложение
+python manage.py makemigrations *name* - создание миграции
+python manage.py migrate - миграция
+python manage.py sqlmigrate servisrecruting 0001 - посмотреть миграцию
+
+
+
+python manage.py shell - в консоли ORM Django (работ с БД)
+Команды БД
+a = *name*.objects.all() - привязать к переменной "а" все объекты *name*
+a = *name*.objects.filter(id=1) - показать в переменной "а" все объекты *name* с id=1
+a = *name*.objects.get(id=2) - привязать к переменной "а" все объекты *name* с id=2
+a.id - показать id переменной "а"
+a.*name*_set.all() - показать все объекты привязаные (ForingKey привязкой) к *name* (например все комментарии к статье)
+a.*name*_set.count() - показать количество 
+a.*name*_set.filter( author_name__startswith = "Дж" ) - показать имена авторов статей, имя которых начинаются на "Дж"
+a.*name*_set.create ( author_name = "Джек", comment_text = "ZBS" ) - создать запить в БД (в данном случает создать комментарий ZBS автора Джек к статье)
+a.delete() - удалить все, что в переменной "а"
+a.save() - обновить/сохранить то, что было в переменной "а" в БД
+
+
+
 
 CSS бесплатные шаблоны
 Для установки Bootstrap тебе нужно добавить следующие строки в <head> 
--- pythoncommand.py
+++ pythoncommand.py
@@ -4,4 +4,19 @@ python3 -m http.server 7800 - запустить на порту 7800
 
 
 
+f = open('name file.py', 'w') - открыть файл для записи
+f = open('name file.py', 'r') - открыть файл для чтения. Так же если не указывать режим
+f = open('name file.py', 'a') - открыть файл для добавления
+
+while True: - для чтения файла
+    line = f.readline() - читает файл построчно
+    if len(line) == 0: # Нулевая длина обозначает конец файла (EOF)
+        break
+    print(line, end='')
+
+import pickle - позволяет сохранять любой ОБЪЕКТ PYTHON в файл. И доставать его обратно. 
+f = open(shopelist, 'wb') - СНАЧАЛА open с WB - бинарной записью.
+pickle.dump (shoplist, f)
+shoplist = pickle.load(f)
+
 """
